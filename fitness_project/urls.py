a621d80419d19e3840d302aa748dd14bafcf5a5c
Xavier <xavieralves162@gmail.com>
2025-10-16 11:14:18 +0100

    Added paths for user authentication

--- fitness_project/urls.py
+++ fitness_project/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from workouts import views as workout_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+
+    # Authentication routes
+    path("login/", auth_views.LoginView.as_view(template_name="auth/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
+    path("register/", workout_views.register, name="register"),
+
+    # Main app
+    path("", include("workouts.urls")),
 ]
