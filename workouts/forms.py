ab22df4059278352994c23da7ad7288c00ec734c
Xavier <xavieralves162@gmail.com>
2025-11-03 12:18:52 +0000

    Added forms.py for user form

---
+++ workouts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Profile
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["age", "height", "weight", "preferred_units", "level"]
+        widgets = {
+            "age": forms.NumberInput(attrs={"class": "form-control"}),
+            "height": forms.NumberInput(attrs={"class": "form-control", "step": "0.1"}),
+            "weight": forms.NumberInput(attrs={"class": "form-control", "step": "0.1"}),
+            "preferred_units": forms.Select(attrs={"class": "form-control"}),
+            "level": forms.Select(attrs={"class": "form-control"}),
+        }
