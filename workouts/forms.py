fc181305819a46ddd79b8d5cdd644ed04dc9cbf2
Xavier <xavieralves162@gmail.com>
2025-11-06 15:04:52 +0000

    Added forms for Exercise

--- workouts/forms.py
+++ workouts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Profile
+from .models import Profile, Exercise
 
 class ProfileForm(forms.ModelForm):
     class Meta:
@@ -11,4 +11,14 @@ class ProfileForm(forms.ModelForm):
             "weight": forms.NumberInput(attrs={"class": "form-control", "step": "0.1"}),
             "preferred_units": forms.Select(attrs={"class": "form-control"}),
             "level": forms.Select(attrs={"class": "form-control"}),
+        }
+
+class ExerciseForm(forms.ModelForm):
+    class Meta:
+        model = Exercise
+        fields = ["name", "category", "description"]
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "category": forms.Select(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control", "rows": 3}),
         }
